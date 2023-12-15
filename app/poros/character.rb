abba0c0b8497a75a6098939f19151d3275989c63
Sam Tran <samuelttran@gmail.com>
2023-12-15 10:29:05 -0700

    Update poro to account for None

--- app/poros/character.rb
+++ app/poros/character.rb
@@ -9,9 +9,29 @@ class Character
   def initialize(data)
     @id = data[:_id]
     @name = data[:name]
-    @allies = data[:allies] #
-    @enemies = data[:enemies] #
-    @affiliations = data[:affiliation] #
+    @allies = allies_info(data)
+    @enemies = enemies_info(data)
+    @affiliations = data[:affiliation]
     @search_nation_count = data[:nation_count] # Should not go here, but to make it easy for now
   end
+
+  private
+    
+    def allies_info(data)
+      allies_data = data[:allies]
+      if allies_data.nil? || allies_data.empty?
+        "None"
+      else
+        allies_data
+      end
+    end
+
+    def enemies_info(data)
+      enemies_data = data[:enemies]
+      if enemies_data.nil? || enemies_data.empty?
+        "None"
+      else
+        enemies_data
+      end
+    end
 end
--- spec/poros/character_spec.rb
+++ spec/poros/character_spec.rb
@@ -33,4 +33,26 @@ RSpec.describe Character do
     expect(character.affiliations).to eq(" Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
     expect(character.search_nation_count).to eq(97) # check this later
   end
+
+  it "returns 'None' for allies and enemies if none exist" do
+    data = {
+      _id: "5cf5679a915ecad153ab68da",
+      allies: [],
+      enemies: [],
+      photoUrl: "https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941",
+      name: "Azula",
+      affiliation: " Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)",
+      nation_count: 97
+  }
+  
+    character = Character.new(data)
+
+    expect(character).to be_a(Character)
+    expect(character.id).to eq("5cf5679a915ecad153ab68da")
+    expect(character.name).to eq("Azula")
+    expect(character.allies).to eq("None")
+    expect(character.enemies).to eq("None")
+    expect(character.affiliations).to eq(" Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
+    expect(character.search_nation_count).to eq(97) # check this later
+  end
 end
